dfe4f15eba1a9a82c7a51e31feb12d65b536446c
Farah <akirdelouafifarah@gmail.com>
2025-10-27 22:39:38 +0100

    changing files

---
+++ miniprojet/programme/miniprojet_script.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+FICHIER_URL=$1
+
+# vérifier si j'ai bien fourni l'argument
+if [ $# -ne 1 ]; then
+	echo "Erreur : oops, as-tu oublié un argument ?"
+	exit 1
+fi 
+
+# vérifier que le fichier existe bel et bien
+if [ ! -f "$FICHIER_URL" ]; then
+	echo "Erreur : le fichier '$FICHIER_URL' n'existe pas sniff"
+	exit 1
+fi
+
+# lecture du fichier ligne par ligne
+n=1
+while read url; do
+		# pour récup le code HTTP
+  code=$(curl -o /dev/null -s -w "%{http_code}" "$url")
+		# pour récup le contenu de la page
+  contenu=$(curl -s "$url")
+		# pour chercher l'encodage
+  encodage=$(echo "$contenu" | grep -oi 'charset=[^"]*' | head -n1 | cut -d= -f2)
+		# pour calculer le nb de mots
+  mots=$(echo "$contenu" | wc -w)
+  echo -e "$n\t$url\t$code\t${encodage:-inconnu}\t$mots"
+  ((n++))
+done < "$FICHIER_URL"
+
+
